a8f4534d112f07e1e851248a417b6a7de09fc7c7
Lenart <lenart.erznoznik@gmail.com>
2026-07-18 09:27:53 +0200

    Placano: Neza Berce; vpis rezultatov: 24a 39:60, 24b 41:39

--- data.js
+++ data.js
@@ -3,7 +3,7 @@
 
 // ===== IMENA: placeholderje zamenjaj s pravimi imeni =====
 const IMENA = {
-  "A": "NEŽA BERCE",
+  "A": "NEŽA BERCE", // PLAČANO
   "B": "NINA TOMAŽIN",
   "C": "LIA BERCE",
   "D": "MARUŠA DOLINAR", // PLAČANO
@@ -85,8 +85,8 @@ const REZULTATI = {
   "22b": "40:36",          // 07:30 ig.2 | LOTI GRUŠKOVNJAK & URBAN JEREB — TIA MOČNIK & DAVID ŠENK
   "23a": "49:36",          // 08:10 ig.1 | LOTI GRUŠKOVNJAK & DOMEN DOLENC — LENA KRISTAN & JAN DOLENC
   "23b": "31:40",          // 08:10 ig.2 | NINA TOMAŽIN & BRIC KROLNIK — NEŽA BERCE & KRIŠTOF GANTAR
-  "24a": "",          // 08:50 ig.1 | LIZA POLJANŠEK & KRIŠTOF GANTAR — LIA BERCE & DOMEN DOLENC
-  "24b": "",          // 08:50 ig.2 | MAŠA VEGELJ & LUYGGY — MARUŠA DOLINAR & JAKA KOPAČ
+  "24a": "39:60",          // 08:50 ig.1 | LIZA POLJANŠEK & KRIŠTOF GANTAR — LIA BERCE & DOMEN DOLENC
+  "24b": "41:39",          // 08:50 ig.2 | MAŠA VEGELJ & LUYGGY — MARUŠA DOLINAR & JAKA KOPAČ
   "25a": "",          // 09:30 ig.1 | LENA KRISTAN & DAVID ŠENK — EMA KAVČIČ & JAN DOLENC
   "25b": "",          // 09:30 ig.2 | NINA TOMAŽIN & JAN GOVEKAR — LIA BERCE & JUŠ BURJEK
   "26a": "",          // 10:10 ig.1 | ERIKA MOHORIČ & BRIC KROLNIK — TIA MOČNIK & MATIC ENIKO
